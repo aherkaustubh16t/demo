9502d9ef9e196d33781cf8831b929d7b910ed1e2
Kaustubh Aher <aherkaustubhramesh16@gmail.com>
2025-03-14 14:37:51 +0530

    Add kasutubh in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <div>Kaustubh</div>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
